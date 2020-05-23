46373649a98e41c523928496455e2f9a8b445618
holmanej <holmanej@gmail.com>
2020-05-23 13:27:09 -0400

    Init

---
+++ OrbitCalculator/Orbit.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitCalculator
+{
+    class Orbit
+    {
+        double Mu;
+        public double a;
+        public double e;
+
+        public Orbit(double[] velocity, double[] position, double mass)
+        {
+            Mu = mass * 6.674e-11;
+            a = Geta(Mu, velocity, position);
+            e = Gete(Mu, velocity, position);
+        }
+
+        public double GetE(int t)
+        {
+            double E = 0;
+            for (int i = 0; i <= t; i++)
+            {
+                E -= (E - e * Math.Sin(E) - i * Math.Sqrt(Mu / Math.Pow(a, 3))) / (1 - e * Math.Cos(E));
+            }
+            return E;
+        }
+
+        public double Getr(int t)
+        {
+            double E = GetE(t);
+            return (a * (1 - Math.Pow(e, 2))) / (1 + e * ((Math.Cos(E) - e) / (1 - e * Math.Cos(E))));
+        }
+
+        public double Getv(int t)
+        {
+            double r = Getr(t);
+            return Math.Sqrt(Mu * (2 / r - 1 / a));
+        }
+
+        public double GetRp()
+        {
+            return a * (1 - e);
+        }
+
+        public double GetRa()
+        {
+            return a * (1 + e);
+        }
+
+        public double GetT()
+        {
+            return (2 * Math.PI * Math.Sqrt(Math.Pow(a, 3) / Mu));
+        }
+
+        static double Magnitude(double[] v)
+        {
+            double m = Math.Sqrt(Math.Pow(v[0], 2) + Math.Pow(v[1], 2) + Math.Pow(v[2], 2));
+            return m;
+        }
+
+        static double Gete(double mu, double[] v, double[] r)
+        {
+            double temp1 = Math.Pow(Magnitude(v), 2) - mu / Magnitude(r);
+            double temp2 = v[0] * r[0] + v[1] * r[1] + v[2] * r[2];
+            double[] eVec = { (r[0] * temp1 - v[0] * temp2) / mu, (r[1] * temp1 - v[1] * temp2) / mu, (r[2] * temp1 - v[2] * temp2) / mu };
+            return Magnitude(eVec);
+        }
+
+        static double Geta(double mu, double[] v, double[] r)
+        {
+            double E = (Math.Pow(Magnitude(v), 2) / 2) - (mu / Magnitude(r));
+            return -mu / (2 * E);
+        }
+    }
+}
---
+++ OrbitCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitCalculator
+{
+    class Program
+    {
+        public static double Mu = 1.327e20;
+
+        static void Main(string[] args)
+        {
+            double axis = 0;
+            for (int i = 0; axis < 1.5e11; i++)
+            {
+                axis = CalcMajorAxis(3600 * i, 3000);
+                Debug.WriteLine(i + " : " + axis / 1000000000);
+            }
+            CalcOrbit(3000, 3600 * 3135, 2e30);
+            CalcOrbit(1500, 86400 * 700, 2e30);
+
+            Console.ReadKey();
+        }
+
+        static void CalcOrbit(double velocity, double period, double mass)
+        {
+            double axis = CalcMajorAxis(period, velocity);
+            double[] v = { 0, velocity, 0 };
+            double[] p = { axis, 0, 0 };
+            Orbit Roid = new Orbit(v, p, mass);
+            Console.WriteLine("e: " + Roid.e);
+            Console.WriteLine("a: " + (Roid.a / 1000000000) + " M Km");
+            Console.WriteLine("Period: " + (Roid.GetT() / 3600) + " hours");
+            Console.WriteLine("Perihelion: " + (Roid.GetRp() / 1000) + " Km");
+            Console.WriteLine("Aphelion: " + (Roid.GetRa() / 1000000000) + " M Km");
+
+            Console.WriteLine("Sun Orbit Dist: " + (Roid.GetRp() / 1000) + " Km");
+            Console.WriteLine("Sun speed: " + (Roid.Getv(0) / 1000) + " Km/s");
+            Console.WriteLine("c%: " + (Roid.Getv(0) / 3000000) + "%");
+            Console.WriteLine("Object Orbit Dist: " + ((Roid.GetRa() - 1.5e11) / 1000) + " Km");
+            Console.WriteLine("Object speed: " + Roid.Getv(Convert.ToInt32(Roid.GetT()) / 2));
+            Console.WriteLine("\r\n");
+        }
+
+        static double CalcMajorAxis(double period, double velocity)
+        {
+            double temp = period / 6.28;
+            temp = Math.Pow(temp, 2);
+            temp *= Mu;
+            temp = Math.Pow(temp, (double)1 / 3);
+            temp *= (-2 / Mu);
+            temp = 1 / temp;
+            temp = (Math.Pow(velocity, 2) / 2) - temp;
+            temp = 1 / temp;
+            temp *= Mu;
+            return temp;
+        }
+    }
+}
